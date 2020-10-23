2ebd69b9e709248accf16da5843f444382e9d98b
titiscms <titisbackup@gmail.com>
2020-10-22 22:06:13 -0300

    Configuração inicial do projeto

---
+++ src/main/java/br/com/mjv/noticias/TaskNoticiassApplication.java
@@ -0,0 +1,13 @@
+package br.com.mjv.noticias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskNoticiassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskNoticiassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mjv/noticias/TaskNoticiassApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mjv.noticias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskNoticiassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
